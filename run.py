7f7d4fe6fbc07d80816562e6b68e39fc447867ac
reireirei1221 <reireirei1221@icloud.com>
2023-05-20 14:11:52 +0900

    相対パスと絶対パスを導入しました

--- run.py
+++ run.py
@@ -14,8 +14,6 @@ from googleapiclient.errors import HttpError
 # If modifying these scopes, delete the file token.json.
 SCOPES = ['https://www.googleapis.com/auth/calendar']
 
-
-
 def init():
     """Shows basic usage of the Google Calendar API.
     Prints the start and name of the next 10 events on the user's calendar.
@@ -42,6 +40,7 @@ def init():
 
 def create_event(creds, calendar_id, event_summary, year, month, date, start_time, end_time):
     service = build('calendar', 'v3', credentials=creds)
+    print("create_event")
 
     if not start_time and not end_time:
         event = {
@@ -84,11 +83,29 @@ def create_event(creds, calendar_id, event_summary, year, month, date, start_tim
     # 予定を作成
     try:
         event = service.events().insert(calendarId=calendar_id, body=event).execute()
-        print(f"Event created: {event.get('htmlLink')}")
+        event_id = event.get('id')
+        print(f"Event created: {event_id}")
     except HttpError as e:
-        print(f"Error: {e}")
+        print(f"Error: event is not created. {e}")
     
     return
+
+
+def num_to_week(num):
+    if num == 0:
+        return "Mon"
+    elif num == 1:
+        return "Tue"
+    elif num == 2:
+        return "Wed"
+    elif num == 3:
+        return "Thu"
+    elif num == 4:
+        return "Fri"
+    elif num == 5:
+        return "Sat"
+    elif num == 6:
+        return "Sun"
 
 def remove_event(creds, calendar_id, event_id):
     service = build('calendar', 'v3', credentials=creds)
@@ -98,7 +115,7 @@ def remove_event(creds, calendar_id, event_id):
         service.events().delete(calendarId=calendar_id, eventId=event_id).execute()
         print(f"Event deleted")
     except HttpError as e:
-        print(f"Error: {e}")
+        print(f"No event is deleted. {e}")
 
     return
 
@@ -114,6 +131,9 @@ def list_events_by_date(creds, calendar_id, year, month, date):
     start_dt = jst.localize(datetime.datetime(year, month, date, 0, 0, 0))
     end_dt = jst.localize(datetime.datetime(year, month, date, 23, 59, 59))
     
+    week = start_dt.weekday()
+    week = num_to_week(week)
+    
     # JSTでの開始日時と終了日時を文字列に変換
     start_str = start_dt.astimezone(pytz.utc).isoformat()
     end_str = end_dt.astimezone(pytz.utc).isoformat()
@@ -127,29 +147,10 @@ def list_events_by_date(creds, calendar_id, year, month, date):
         orderBy='startTime'
     ).execute()
 
-    # events_result = service.events().list(
-    #     calendarId=calendar_id,
-    #     timeMin=f'{year}-{month}-{date}T00:00:00Z',
-    #     timeMax=f'{year}-{month}-{date}T23:59:59Z',
-    #     singleEvents=True,
-    #     orderBy='startTime'
-    # ).execute()
-
     events = events_result.get('items', [])
-
-    # for event in events:
-    #     start = event.get('start', {}).get(
-    #         'dateTime', event.get('start', {}).get('date', ''))
-    #     end = event.get('end', {}).get(
-    #         'dateTime', event.get('end', {}).get('date', ''))
-    #     summary = event.get('summary', '')
-    #     output = f'{start} ~ {end} {summary}'
-    #     output = output.replace('T', ' ')
-    #     output = output.replace(':00+09:00', '')
-    #     print(output)
     
     # 年-月-日を出力
-    print(f'{year}-{month}-{date}')
+    print(f'{year}-{month}-{date}-{week}')
 
     for event in events:
         start = event.get('start', {}).get(
@@ -187,9 +188,45 @@ def move_to_parent_directory(current_year, current_month, current_date):
         print("You are already at the top directory")
     return current_year, current_month, current_date
 
+def whether_absolute_path(command):
+    if not len(command) == 10:
+        return False
+    else:
+        if command[4] == '/' and command[7] == '/':
+            if command[0:4].isdecimal() and command[5:7].isdecimal() and command[8:10].isdecimal():
+                if int(command[5:7]) > 12 or int(command[8:10]) > 31:
+                    return False
+                return True
+            else:
+                return False
+        else:
+            return False
+        
+def whether_half_absolute_path(current_year, command):
+    if current_year == None:
+        return False
+    if not len(command) == 5:
+        return False
+    else:
+        if command[2] == '/':
+            if command[0:2].isdecimal() and command[3:5].isdecimal():
+                if int(command[0:2]) > 12 or int(command[3:5]) > 31:
+                    return False
+                return True
+            else:
+                return False
+
 def change_directory(current_year, current_month, current_date, command):
-    print(command)
-    if command.startswith('...'):
+    if whether_absolute_path(command):
+        current_year = command[0:4]
+        current_month = command[5:7]
+        current_date = command[8:10]
+        return current_year, current_month, current_date
+    elif whether_half_absolute_path(current_year, command):
+        current_month = command[0:2]
+        current_date = command[3:5]
+        return current_year, current_month, current_date
+    elif command.startswith('...'):
         if len(command) == 3:
             current_year, current_month, current_date = move_to_parent_directory(current_year, current_month, current_date)
             return current_year, current_month, current_date
@@ -353,22 +390,6 @@ def get_week_events(creds, calendar_id, year, month, week):
                 print(f'{time_range} {summary} {id}')
     return
 
-def num_to_week(num):
-    if num == 0:
-        return "Mon"
-    elif num == 1:
-        return "Tue"
-    elif num == 2:
-        return "Wed"
-    elif num == 3:
-        return "Thu"
-    elif num == 4:
-        return "Fri"
-    elif num == 5:
-        return "Sat"
-    elif num == 6:
-        return "Sun"
-
 def get_one_week_events(creds, calendar_id, year, month, week_num):
     jst = pytz.timezone('Asia/Tokyo')
     service = build('calendar', 'v3', credentials=creds)
@@ -484,10 +505,14 @@ def main(creds):
                 if current_year and current_month:
                     get_week_events(creds, current_calendar_id, current_year, current_month, week)
                     print()
+                else:
+                    print("You must specify year and month")
             elif week in ["1", "2", "3", "4", "5"]:
                 if current_year and current_month:
                     get_one_week_events(creds, current_calendar_id, current_year, current_month, int(week))
                     print()
+                else:
+                    print("You must specify year and month")
             else:
                 print("Invalid command")
         
@@ -495,14 +520,18 @@ def main(creds):
         elif command.startswith("ls"):
             command = command[3:]
             ls_commands = command.split(" ")
-            for ls_command in ls_commands:                              
+            for ls_command in ls_commands:    
+                print()                          
                 year, month, date = change_directory(current_year, current_month, current_date, ls_command)
                 list_events_by_date(creds, current_calendar_id, year, month, date)
+            print()
         
         # 予定をidで削除 
         if command.startswith("rm"):
-            rm_command = command[3:]
-            remove_event(creds, current_calendar_id, rm_command)
+            command = command[3:]
+            rm_commands = command.split(" ")
+            for rm_command in rm_commands:
+                remove_event(creds, current_calendar_id, rm_command)
 
         if command.startswith("cd"):
             mv_command = command[3:]
@@ -511,55 +540,64 @@ def main(creds):
         # create_event(creds, calendar_id, event_summary, year, month, date, start_time, end_time)
                 
         if command.startswith("add"):
-            add_command = command.split(" ")
+            add_command = command[4:]
+            add_command = add_command.split(" ")
             if current_year and current_month:
+                if len(add_command) == 1:
+                    print("You need to input summary")
+                    continue
                 if len(add_command) == 2:
                     # 終日の場合
+                    year, month, date = change_directory(current_year, current_month, current_date, add_command[0])
                     summary = add_command[1]
-                    create_event(creds, current_calendar_id, summary, current_year, current_month, current_date, None, None)
+                    create_event(creds, current_calendar_id, summary, year, month, date, None, None)
                 elif len(add_command) == 3:
                     # 開始時間のみ指定
-                    start_time = add_command[1]
+                    year, month, date = change_directory(
+                        current_year, current_month, current_date, add_command[0])
+                    time = add_command[1]
                     summary = add_command[2]
-                    if ":" not in start_time:
-                        print("Invalid time format")
-                        continue
-                    if len(start_time) != 5:
-                        print("Invalid time format")
-                        continue
-                    start_hour = int(start_time[:2])
-                    start_minute = int(start_time[3:])
-                    if start_hour > 23 or start_hour < 0 or start_minute > 59 or start_minute < 0:
-                        print("Invalid time format")
-                        continue
-                    create_event(creds, current_calendar_id, summary, current_year, current_month, current_date, start_time, None)
-                elif len(add_command) == 4:
-                    # 開始時間と終了時間の指定
-                    start_time = add_command[1]
-                    end_time = add_command[2]
-                    summary = add_command[3]
-                    if ":" not in start_time or ":" not in end_time:
-                        print("Invalid time format")
-                        continue
-                    if len(start_time) != 5 or len(end_time) != 5:
-                        print("Invalid time format")
-                        continue
-                    if start_time > end_time:
-                        print("Invalid time format")
-                        continue
-                    start_hour = int(start_time[:2])
-                    start_minute = int(start_time[3:])
-                    end_hour = int(end_time[:2])
-                    end_minute = int(end_time[3:])
-                    if start_hour > 23 or start_hour < 0 or start_minute > 59 or start_minute < 0:
-                        print("Invalid time format")
-                        continue
-                    if end_hour > 23 or end_hour < 0 or end_minute > 59 or end_minute < 0:
-                        print("Invalid time format")
-                        continue
-                    create_event(creds, current_calendar_id, summary, current_year, current_month, current_date, start_time, end_time)
+                    if len(time) == 5:
+                        start_time = time
+                        end_time = None
+                        if ":" not in start_time:
+                            print("You need to include ':' in start time")
+                            continue
+                        if len(start_time) != 3:
+                            print("You need to add '0' to the time")
+                            continue
+                        start_hour = int(start_time[:2])
+                        start_minute = int(start_time[3:])
+                        if start_hour > 23 or start_hour < 0 or start_minute > 59 or start_minute < 0:
+                            print("You need to input valid time")
+                            continue
+                        create_event(creds, current_calendar_id, summary, year, month, date, start_time, end_time)
+                    elif len(time) == 11:
+                        start_time = time[:5]
+                        end_time = time[6:]
+                        if ":" not in start_time or ":" not in end_time:
+                            print("You need to include ':' in start time and end time")
+                            continue
+                        if len(start_time) != 5 or len(end_time) != 5:
+                            print("You need to add '0' to the time")
+                            continue
+                        if start_time > end_time:
+                            print("Start time must be earlier than end time")
+                            continue
+                        start_hour = int(start_time[:2])
+                        start_minute = int(start_time[3:])
+                        end_hour = int(end_time[:2])
+                        end_minute = int(end_time[3:])
+                        if start_hour > 23 or start_hour < 0 or start_minute > 59 or start_minute < 0:
+                            print("You need to input valid time")
+                            continue
+                        if end_hour > 23 or end_hour < 0 or end_minute > 59 or end_minute < 0:
+                            print("You need to input valid time")
+                            continue
+                        create_event(creds, current_calendar_id, summary, year, month, date, start_time, end_time)
+
             else:
-                print("Invalid command")
+                print("You need to specify year and month")
 
         if command == "clear":
             os.system('clear')
